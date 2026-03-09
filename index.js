8721e6ab3ff163f3fcd5fb9f0006c48bc48a2b64
Priyadharshini H <e0423030@sriher.edu.in>
2026-03-09 21:52:35 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const connectDB = require("./config/db")
 const userApi = require("./api/userApi")
 const requestApi = require("./api/requestApi")
 
 const app = express()// app will create
-
+app.use(cors())
 app.use(express.json())
 
 connectDB()
